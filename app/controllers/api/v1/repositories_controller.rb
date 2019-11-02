3b6ffa925ee3162cd90304240aa7865cc385bf19
Daniel Belarmino <belarba@gmail.com>
2019-11-02 12:42:22 -0300

    acessing the github app

--- app/controllers/api/v1/repositories_controller.rb
+++ app/controllers/api/v1/repositories_controller.rb
@@ -3,9 +3,14 @@ module Api
     class RepositoriesController < ApplicationController  
 			before_action :authorize_request, except: :create
 			before_action :find_user, except: %i[create index]
-			
+
 			def index
-				render json: {status: 'SUCCESS', message:'Artigos carregados'},status: :ok
+				require "json"
+
+				response = RestClient.get "https://api.github.com/users/lewagon/repos"
+				repos = JSON.parse(response)
+				render json: repos
+				# render json: {status: 'SUCCESS', message:'Artigos carregados'},status: :ok
 			end 
 		end
 	end
